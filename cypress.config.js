7d2eea9e31e4ea6eea83bde9705773b660c76efe
Shahd Alhammad <sh.alhammad@tamkeentech.sa>
2025-10-18 18:01:48 +0300

    cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+     
+    },
+  },
+});
